aac616c9db034f5f22c5dfb5198199be72b6bbe0
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-08-25 16:43:56 -0700

    1

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * add_node - Adds some new variabls to linked list
+  * @head: The basic linked list
+  * @str: So bad to add any value
+  *
+  * Return: The new list is NULL
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *num;
+
+	if (head != NULL && str != NULL)
+	{
+		num = malloc(sizeof(list_t));
+		if (num == NULL)
+		return (NULL);
+
+		num->str = strdup(str);
+		num->len = _strlen(str);
+		num->next = *head;
+		*head = num;
+		return (num);
+	}
+	return (0);
+}
+/**
+  * _strlen - Returns the length of a string
+  * @s: so bad to count
+  *
+  * Return: Always (0)
+*/
+int _strlen(const char *s)
+{
+	int g = 0;
+
+	while (*s)
+	{
+		g++;
+		s++;
+	}
+	return (g);
+}
