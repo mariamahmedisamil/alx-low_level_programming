2fc35db8dadb53aaab4844d8baebf35cfc450ebb
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-08-25 16:48:23 -0700

    020

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,20 +26,3 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	return (0);
 }
-/**
-  * _strlen - Returns the length of a string
-  * @s: so bad to count
-  *
-  * Return: Always (0)
-*/
-int _strlen(const char *s)
-{
-	int g = 0;
-
-	while (*s)
-	{
-		s++;
-		g++;
-	}
-	return (g);
-}
