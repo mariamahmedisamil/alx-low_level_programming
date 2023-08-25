9a3e178a3b6fec5270aec94d3797fc91237174f1
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-08-25 16:04:29 -0700

    maria

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,19 +5,17 @@
 * @h: pointer to the number of nodes at list_h
 * Return: the number of nodes
 */
-size_t print_list(const list_t *h)
+size_t print_list(const list_t *h);
 {
- size_t m = 0;
-
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- m++;
- }
-
- return (m);
+size_t m = 0;
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+m++;
+}
+return (m);
 }
