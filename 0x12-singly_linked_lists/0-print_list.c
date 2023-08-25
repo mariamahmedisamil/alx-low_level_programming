bbb85afddd120264e2a575fe63804f603e2778f8
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-08-25 16:20:30 -0700

    kuhg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 /**
-* print_list - prints all the number of nodes of list_h
-* @h: pointer to the number of nodes at list_h
-* Return: the number of nodes
+  * print_list - Prints all numbers in alx
+  * @h: Teacher point on something
+  *
+  * Return: The number of nodes to the main list
 */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-size_t m = 0;
-while (h)
-{
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-m++;
-}
-return (m);
+	size_t maria = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		maria++;
+	}
+
+	return (maria);
 }
