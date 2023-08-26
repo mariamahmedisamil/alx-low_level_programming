74ec760e81f7c14972983eabc61187e5be5c1bb7
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-08-25 17:28:07 -0700

    3232

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
 /**
   * add_node - Adds a new node at the beginning of main list
   * @head: The original linked list
-  * @str: The string to add to main list 
+  * @str: The string to add to main list
   *
   * Return: The address of the new list or NULL if it failed
   */
