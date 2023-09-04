12e958eff2f8df5aaf78c88181ddc4a4a2a47837
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-09-04 15:29:22 -0700

    32145

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,34 +7,34 @@
 */
 int _strlen(char *s)
 {
-	int c = 0;
+	int i = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		c++;
-	return (c);
+		i++;
+	return (i);
 }
 /**
 * create_file - Add new files
 * @filename: add name to the new file
-* @text_content: add some text to be readn
+* @text_content: add some text to be readen
 *
-* Return: 1 if it success 0 if it errored
+* Return: 1 if it success, 0 if it errored
 */
 int create_file(const char *filename, char *text_content)
 {
-	int id;
-	ssize_t sumt = 0, len = _strlen(text_content);
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	id = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (id == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 	if (len)
-		sumt = write(fd, text_content, len);
-	close(id);
-	return (sumt == len ? 1 : -1);
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
