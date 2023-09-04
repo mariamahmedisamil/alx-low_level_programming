ead7e0cde978ebb6aff1f23889872e7029c35a7a
mariamahmedisamil <mariamzainhelal84@gmail.com>
2023-09-04 13:59:23 -0700

    3699852369523

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,14 +7,14 @@
 */
 int _strlen(char *s)
 {
-	int r = 0;
+        int r = 0;
 
-	if (!a)
-		return (0);
+        if (!a)
+                return (0);
 
-	while (*a++)
-		r++;
-	return (r);
+        while (*a++)
+                r++;
+        return (r);
 }
 /**
 * create_file - Add new files
@@ -25,16 +25,16 @@ int _strlen(char *s)
 */
 int create_file(const char *filename, char *text_content)
 {
-	int id;
-	ssize_t sumt = 0, temp = _strlen(text_content);
+        int id;
+        ssize_t sumt = 0, temp = _strlen(text_content);
 
-	if (!filename)
-		return (-1);
-	id = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (id == -1)
-		return (-1);
-	if (temp)
-		sumt = write(id, text_content, temp);
-	close(id);
-	return (sumt == temp ? 1 : -1);
+        if (!filename)
+                return (-1);
+        id = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (id == -1)
+                return (-1);
+        if (temp)
+                sumt = write(id, text_content, temp);
+        close(id);
+        return (sumt == temp ? 1 : -1);
 }
